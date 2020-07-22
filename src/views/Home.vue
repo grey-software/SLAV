452ed6db88ac75d52996d8d8ff8af0971ee640d7
Osama Saleh <osama.saleh@mail.utoronto.ca>
2020-07-22 13:01:51 -0400

    BFS refactor to VueX

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,7 +41,6 @@
       </v-row>
     </v-toolbar>
     <v-container class="home p-4">
-
       <div class="subtitle">
         <p>{{ selectionStateLabels[selectionState] }}</p>
       </div>
@@ -198,13 +197,12 @@ export default {
       "setStartCoors",
       "setDestCoors",
       "setCurrCoors",
-      "resetGrid",
-     
+      "resetGrid"
     ]),
     ...mapActions([
-      "createGraph", 
-      "runAlgorithm", 
-      "onGridCellClicked", 
+      "createGraph",
+      "runAlgorithm",
+      "onGridCellClicked",
       "onGridCellHover",
       "dfs",
       "dfsVisit",
@@ -214,9 +212,7 @@ export default {
       "best_first",
       "drawPath",
       "reset"
-
-      
-      ]),
+    ]),
     addWall(wallCoordinate) {
       this.graph[wallCoordinate].state = WALL;
     },
@@ -260,32 +256,6 @@ export default {
         }
       }
     },
-
-    // async runAlgorithm(algorithm) {
-
-    //   const startNode = this.graph[`(${this.startX},${this.startY})`];
-    //   const endNode = this.graph[`(${this.destX},${this.destY})`];
-    //   this.setIsAlgorithmRunning(true);
-    //   console.log(`Running algorithm: ${algorithm}`);
-    //   if (algorithm == "search/breadth-first") {
-    //     this.path = await this.bfs(this.graph, startNode, endNode);
-    //   } else if (algorithm == "search/depth-first") {
-    //     this.path = await this.dfs(this.graph, startNode, endNode);
-    //   } else if (algorithm == "search/a-star") {
-    //     console.log("calling a star");
-    //     this.path = await this.aStar(this.graph, startNode, endNode);
-    //   } else if (algorithm == "search/best-first") {
-    //     this.path = await this.best_first(this.graph, startNode, endNode);
-    //   }
-    //   for (let i = this.path.length - 1; i >= 0; i--) {
-    //     this.path[i].state = PATH;
-    //     await sleep(50);
-    //   }
-    //   this.setIsAlgorithmRunning(true);
-    //   this.setIsAlgorithmFinished(true);
-    //   // Convert to a mutation
-    //   this.setSelectionState("pick-start");
-    // },
     async dfs(graph, startNode, endNode) {
       // Perform DFS at start Node
       var path = await this.dfsVisit(startNode, startNode, endNode);
@@ -322,114 +292,6 @@ export default {
     onVizClicked() {
       this.runAlgorithm({ algorithm: this.currentAlgorithm });
     },
-    async bfs(graph, startNode, endNode) {
-      const queue = new Queue();
-      startNode.dist = 0;
-      queue.enqueue(startNode);
-      this.currentAlgorithmStruct = queue;
-      startNode.queueState = true;
-      //this.queueNodes[`${startNode.x},${startNode.y}`] = startNode;
-      while (!queue.isEmpty() && this.isAlgorithmRunning) {
-        //console.log(`Length${queue.length()}`);
-        const currentNode = queue.dequeue();
-        currentNode.queueState = false;
-        //this.queueNodes[`${currentNode.x},${currentNode.y}`] = "";
-        await sleep(this.delayFactor / this.vizSpeed);
-        if (currentNode.state != WALL) {
-          //visited.push(currentNode);
-          if (this.isAlgorithmRunning) {
-            currentNode.state = VISITED;
-          }
-        }
-        var iterNode = currentNode;
-        if (iterNode.x == endNode.x && iterNode.y == endNode.y) {
-          return this.drawPath(iterNode, startNode);
-        }
-        //View node's color should observe model node's color
-        for (const neighbourCoors of currentNode.adj) {
-          const neighbour = this.graph[neighbourCoors];
-          //Only interact with non-wall nodes
-          if (neighbour.state != WALL) {
-            //Visit
-            if (neighbour.parent == null) {
-              neighbour.parent = currentNode;
-            }
-            if (neighbour.state == EMPTY) {
-              if (this.isAlgorithmRunning) {
-                neighbour.state = VISITED;
-              }
-              neighbour.dist = currentNode.dist + 1;
-              queue.enqueue(neighbour);
-              neighbour.queueState = true;
-              //this.queueNodes[`${neighbour.x},${neighbour.y}`] = neighbour;
-            }
-          }
-        }
-        // console.log(queue.items)
-        currentNode.state = EXPLORED;
-      }
-    },
-
-    async aStar(graph, startNode, endNode) {
-      const nodesToExplore = [];
-      var exploredNodes = [];
-      var currentNodeIndex;
-      nodesToExplore.push(startNode);
-      var currentNode;
-      var higherPriorityChildExists;
-      var childExplored;
-      while (nodesToExplore.length != 0 && this.isAlgorithmRunning) {
-        // Remove element with least f-value
-        currentNodeIndex = this.findMinNodeIndex(nodesToExplore);
-        currentNode = nodesToExplore[currentNodeIndex];
-        exploredNodes.push(currentNode);
-        nodesToExplore.splice(currentNodeIndex);
-        if (this.isAlgorithmRunning) {
-          currentNode.state = VISITED;
-        }
-        await sleep(this.delayFactor / this.vizSpeed);
-        //Check if we're done
-        if (currentNode.x == endNode.x && currentNode.y == endNode.y) {
-          return this.drawPath(currentNode, startNode);
-        }
-        for (let neighbourCoors of currentNode.adj) {
-          const neighbour = this.graph[neighbourCoors];
-          if (neighbour.state == WALL) {
-            continue;
-          }
-          if (neighbour.parent == null) {
-            neighbour.parent = currentNode;
-          }
-          higherPriorityChildExists = false;
-          childExplored = false;
-          neighbour.distanceFromStart = currentNode.distanceFromStart + 1;
-          neighbour.distanceToDest =
-            Math.abs(neighbour.x - endNode.x) +
-            Math.abs(neighbour.y - endNode.y);
-          neighbour.nodeCost =
-            neighbour.distanceFromStart + neighbour.distanceToDest;
-          for (let closedChild of exploredNodes) {
-            if (closedChild.x == neighbour.x && closedChild.y == neighbour.y) {
-              childExplored = true;
-            }
-          }
-          for (let openChild of nodesToExplore) {
-            if (openChild.x == neighbour.x && openChild.y == neighbour.y) {
-              if (neighbour.distanceFromStart > openChild.distanceFromStart) {
-                higherPriorityChildExists = true;
-              }
-            }
-          }
-          if (!higherPriorityChildExists && !childExplored) {
-            if (this.isAlgorithmRunning) {
-              neighbour.state = VISITED;
-            }
-            nodesToExplore.push(neighbour);
-          }
-        }
-        currentNode.state = EXPLORED;
-      }
-    },
 
     findMinNodeIndex(lst) {
       var minValue = Infinity;
@@ -443,16 +305,6 @@ export default {
         count += 1;
       }
       return index;
-    },
-
-    drawPath(iterNode, startNode) {
-      //Iterate back up the parents until you find the null parent
-      var path = [];
-      while (!(iterNode.x == startNode.x && iterNode.y == startNode.y)) {
-        path.push(iterNode);
-        iterNode = iterNode.parent;
-      }
-      return path;
     }
   }
 };
